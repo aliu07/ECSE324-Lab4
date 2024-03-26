65b7166d9c1e6b314d592ff46a5755a0d18d9c83
Alexander Liu <alexander_l_22@hotmail.com>
2024-03-26 15:50:23 -0400

    Completed VGA drivers

--- vga.s
+++ vga.s
@@ -1,4 +1,7 @@
 .global _start
+
+.equ PIXEL_BUFFER, 0xC8000000 // Pixel buffer base address
+
 _start:
         bl      draw_test_screen
 end:
@@ -9,16 +12,60 @@ end:
 // This subroutine draws a point on the screen at the specified (x, y) coordinates in the indicated color c. 
 // The subroutine should check that the coordinates supplied are valid, i.e., x in [0, 319] and y in [0, 239]. 
 // Hint: This subroutine should only access the pixel buffer.
+// INPUT: A1 -> x-coordinate, A2 -> y-coordinate, A3 -> color half-word  TODO: VALIDATE THESE ARGUMENTS
 VGA_draw_point_ASM:
+    PUSH {V1-V2, LR}
+    // INPUT VALIDATION
+    CMP A1, #0
+    BLT VGA_draw_point_ASM_end // Exit function if x < 0
+    CMP A1, #320
+    BGE VGA_draw_point_ASM_end // Exit function if x > 319
+    CMP A2, #0
+    BLT VGA_draw_point_ASM_end // Exit function if y < 0
+    CMP A2, #239
+    BGT VGA_draw_point_ASM_end // Exit function if y > 239
+    LDR V1, =PIXEL_BUFFER // Load pixel buffer base address
+    MOV V2, A2 // Instantiate offset value to y-coordinate
+    LSL V2, #10 // Shift y-coordinate up 10 bits
+    LSL A1, #1 // Shift x-coordinate up 1 bit temporarily
+    ADD V2, V2, A1 // Add in x-coordinate
+    ASR A1, #1 // Shift x-coordinate back down 1 bit to original value
+    STRH A3, [V1, V2] // Store lower 16 bits in colour register A3 into base address + offset
+    
+    VGA_draw_point_ASM_end:
+        POP {V1-V2, PC}
 
 // This subroutine clears (sets to 0) all the valid memory locations in the pixel buffer. It takes no arguments 
-// and returns nothing. Hint: You can implement this function by calling VGA_draw_point_ASM with a color value 
-// of zero for every valid location on the screen.
+// and returns nothing. 
+// Hint: You can implement this function by calling VGA_draw_point_ASM with a color value of zero for every valid 
+// location on the screen.
 VGA_clear_pixelbuff_ASM:
+    PUSH {LR}
+    MOV A2, #0 // Instantiate row index
+    MOV A3, #0 // Colour value set to 0 to clear buffer
+
+    for_each_row:
+        MOV A1, #0 // Instantiate row index
+        CMP A2, #240
+        BEQ VGA_clear_pixelbuff_ASM_end // Break from inner loop when we have iterated through all rows 0-219
+
+    for_each_col:
+        CMP A1, #320
+        BEQ for_each_row_end // Break from outer loop when we have iterated through all cols 0-319
+        BL VGA_draw_point_ASM // Branch to subroutine to clear buffer at current (x,y) coordinate
+        ADD A1, A1, #1 // Increment col index
+        B for_each_col
+
+    for_each_row_end:
+        ADD A2, A2, #1 // Increment row index
+        B for_each_row
+
+    VGA_clear_pixelbuff_ASM_end:
+        POP {PC}
 
 // This subroutine writes the ASCII code c to the screen at (x, y). The subroutine should check that the 
-// coordinates supplied are valid, i.e., x in [0, 79] and y in [0, 59]. Hint: This subroutine should only 
-// access the character buffer.
+// coordinates supplied are valid, i.e., x in [0, 79] and y in [0, 59]. 
+// Hint: This subroutine should only access the character buffer.
 VGA_write_char_ASM:
 
 // This subroutine clears (sets to 0) all the valid memory locations in the character buffer. It takes no 
@@ -37,10 +84,12 @@ draw_test_screen:
         ldr     r9, .draw_test_screen_L8+4
         ldr     r8, .draw_test_screen_L8+8
         b       .draw_test_screen_L2
+
 .draw_test_screen_L7:
         add     r6, r6, #1
         cmp     r6, #320
         beq     .draw_test_screen_L4
+
 .draw_test_screen_L2:
         smull   r3, r7, r10, r6
         asr     r3, r6, #31
@@ -48,26 +97,28 @@ draw_test_screen:
         lsl     r7, r7, #5
         lsl     r5, r6, #5
         mov     r4, #0
+
 .draw_test_screen_L3:
         smull   r3, r2, r9, r5
         add     r3, r2, r5
         asr     r2, r5, #31
         rsb     r2, r2, r3, asr #9
-        orr     r2, r7, r2, lsl #11
-        lsl     r3, r4, #5
+        orr     r2, r7, r2, lsl #11 // R2 contains red bits, r7 contains blue bits
+        lsl     r3, r4, #5 // Shift green bits
         smull   r0, r1, r8, r3
         add     r1, r1, r3
         asr     r3, r3, #31
         rsb     r3, r3, r1, asr #7
-        orr     r2, r2, r3
-        mov     r1, r4
-        mov     r0, r6
+        orr     r2, r2, r3 // Colour value
+        mov     r1, r4 // Move y-coordinate into R1 (argument for subroutine)
+        mov     r0, r6 // Move x-coordinate into R0 (argument for subroutine)
         bl      VGA_draw_point_ASM
-        add     r4, r4, #1
-        add     r5, r5, #32
+        add     r4, r4, #1 // Increment y-coordinate
+        add     r5, r5, #32 // Loop counter
         cmp     r4, #240
         bne     .draw_test_screen_L3
         b       .draw_test_screen_L7
+
 .draw_test_screen_L4:
         mov     r2, #72
         mov     r1, #5
@@ -118,6 +169,7 @@ draw_test_screen:
         mov     r0, #31
         bl      VGA_write_char_ASM
         pop     {r4, r5, r6, r7, r8, r9, r10, pc}
+
 .draw_test_screen_L8:
         .word   1717986919
         .word   -368140053
